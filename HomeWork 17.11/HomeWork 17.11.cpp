6c9c897e83eb22a204ba055dcb03cda05aef560a
thewhitemage13 <144810207+thewhitemage13@users.noreply.github.com>
2023-11-18 17:46:27 +0100

    Update HomeWork 17.11.cpp

--- HomeWork 17.11/HomeWork 17.11.cpp
+++ HomeWork 17.11/HomeWork 17.11.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -6,11 +6,12 @@ struct DataTime {
 	unsigned short day{};
 	unsigned short month{};
 	long long year{};
+	unsigned short hour{};
+	unsigned short minuts{};
 	string weekday;
 };
 
 struct Check {
-	
 	string name;
 	string adress;
 	string goodsname;
@@ -42,7 +43,7 @@ void InfoChek(CheckDataTime& p)
 
 	cout << "Enter day:";
 	cin >> p.now.day;
-	
+
 	if (p.now.day < 1 || p.now.day > 31)
 	{
 		cout << "Oops, inccorrect value for day!\n";
@@ -51,7 +52,7 @@ void InfoChek(CheckDataTime& p)
 
 	cout << "Enter month:";
 	cin >> p.now.month;
-	
+
 	if (p.now.month < 1 || p.now.month > 12)
 	{
 		cout << "Oops, inccorrect value for month!\n";
@@ -61,11 +62,26 @@ void InfoChek(CheckDataTime& p)
 	cout << "Enter year:";
 	cin >> p.now.year;
 
+	cout << "Enter hour:";
+	cin >> p.now.hour;
+	if (p.now.hour<0 || p.now.hour > 23)
+	{
+		cout << "Oops, inccorrect value for hour!\n";
+		throw "ERROR HOUR!";
+	}
+	cout << "Enter minutes:";
+	cin >> p.now.minuts;
+
+	if (p.now.minuts < 0 || p.now.minuts > 60)
+	{
+		cout << "Oops, inccorrect value for minutes!\n";
+		throw "ERROR MINUTES!";
+	}
 
 	cout << "Enter product name: ";
 	cin >> p.today.goodsname;
 
-	cout<< "Enter price: ";
+	cout << "Enter price: ";
 	cin >> p.today.price;
 
 	if (p.now.month < 0)
@@ -100,7 +116,7 @@ void InfoChek(CheckDataTime& p)
 	int m = p.now.month + 12 * a - 2;
 	int wd = (p.now.day + (31 * m) / 12 + y + y / 4 - y / 100 + y / 400) % 7;
 
-	string weekdays[] = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+	string weekdays[] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 	p.now.weekday = weekdays[wd];
 	p.today.wm = (p.today.price / 100) * p.today.MWST;
 	p.today.netto = p.today.price - p.today.wm;
@@ -112,21 +128,26 @@ void PrintCheck(const CheckDataTime& p)
 	cout << "\t" << p.today.name << "\n";
 	cout << "\t" << p.today.adress << "\n";
 	cout << "\t" << p.today.index << "\n";
-	cout << p.today.goodsname;
-	cout << "\t" <<"Price: " << p.today.price << " A" << "\n";
-	cout << "\t"<<"Your money: " << p.today.mymoney << "\n";
-	cout << "\t"<<"" << p.today.surrenders << "\n";
-	printf("%.2f,\t", p.today.wm);
-	cout << "\n";
-	printf("%.2f,\t", p.today.price);
-	cout << "\n";
-	printf("%.2f,\t", p.today.netto);
-	cout << "\n";
-	printf("%.2f,\t", p.today.surrenders);
-	cout << "\n";
-	cout << p.today.TSEnummer << "\n";
-	cout << p.now.weekday << ", ";
+	cout << "\t" << p.today.goodsname << "\n";
+	cout << "\t";
+	printf("Price: %.0f $", p.today.price);
+	cout << "\n\t";
+	printf("Your money: %.2f $", p.today.mymoney);
+	cout << "\n\t";
+	printf( "Netto: %.2f", p.today.netto);
+	cout << "\t";
+	printf("Brutto: %.2f", p.today.price);
+	cout << "\n\t";
+	printf("MWST: %.2f", p.today.wm);
+	cout << "\n\t";
+	printf("Surrenders: %.2f $", p.today.surrenders);
+	cout << "\n\t";
+	cout <<"TSE: " << p.today.TSEnummer << "\n";
+	cout << "\t";
+	cout <<"Date: " << p.now.weekday << ", ";
 	printf("%02d.%02d.%04d", p.now.day, p.now.month, p.now.year);
+	cout << "\n\t";
+	cout << "Time: " << p.now.hour << ":" << p.now.minuts;
 }
 
 int main()
